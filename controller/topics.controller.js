a25f64c8fa5d66d73021ef9906e58737cc92792e
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-02-19 12:24:07 +0000

    minor change

--- controller/topics.controller.js
+++ controller/topics.controller.js
@@ -1,9 +1,11 @@
 const { selectTopics } = require("../models/topics.model");
 
 function getTopics(req, res, next) {
-  return selectTopics().then((topics) => {
-    res.status(200).send({ topics });
-  });
+  return selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch(next);
 }
 
 module.exports = { getTopics };
