015ef6ff94c54d94d8a88e57df0acd5bf61efbe9
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-02-19 16:07:58 +0000

    minor fix to task 5 query

--- models/articles.model.js
+++ models/articles.model.js
@@ -21,14 +21,13 @@ function selectArticles() {
   return db
     .query(
       `
-    SELECT name AS author, title, articles.article_id, slug AS topic, articles.created_at, 
-    articles.votes, article_img_url, COUNT(comment_id) AS comment_count 
-    FROM articles
-    JOIN users ON articles.author = users.username
-    JOIN topics ON articles.topic = topics.slug
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id, title, users.username, slug
-    ORDER BY created_at DESC;
+        SELECT articles.author, title, articles.article_id, slug AS topic, articles.created_at, 
+        articles.votes, article_img_url, COUNT(comment_id) AS comment_count 
+        FROM articles
+        JOIN topics ON articles.topic = topics.slug
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id, title, slug
+        ORDER BY created_at DESC;
     `
     )
     .then(({ rows }) => {
