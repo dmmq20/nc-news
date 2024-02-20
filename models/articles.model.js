75770b099503137d0892dfa122925ab643bc0310
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-02-20 15:24:21 +0000

    remove commented out code

--- models/articles.model.js
+++ models/articles.model.js
@@ -30,19 +30,7 @@ function selectArticles(topic) {
     queryVals.push(topic);
   }
   query += ` GROUP BY articles.article_id
-      ORDER BY created_at DESC;`;
-  // return db
-  //   .query(
-  //     `
-  //     SELECT articles.author, title, articles.article_id, articles.topic,
-  //     articles.created_at, articles.votes, article_img_url,
-  //     COUNT(comment_id) AS comment_count
-  //     FROM articles
-  //     LEFT JOIN comments ON articles.article_id = comments.article_id
-  //     GROUP BY articles.article_id
-  //     ORDER BY created_at DESC;
-  //     `
-  //   )
+            ORDER BY created_at DESC;`;
   return db.query(query, queryVals).then(({ rows }) => {
     return rows;
   });
